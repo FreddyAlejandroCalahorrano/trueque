d898372401374ab28d5e2a1427d27a9e4283d34f
Freddy_Calahorrano <gfreddy.calahorrano01@epn.edu.ec>
2020-09-03 15:07:18 -0500

    #5 Creacion de Seeding para Productos

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Vaciar la tabla articles.
+        Product::truncate();
+        $faker = \Faker\Factory::create();
+
+            for ($j = 0; $j < 5; $j++) {
+                Product::create([
+                    'title' => $faker->sentence,
+                    'image' => $faker->imageUrl(400,300, null, false),
+                    'value' => $faker->randomDigit,
+                ]);
+            }
+        }
+    }
+
